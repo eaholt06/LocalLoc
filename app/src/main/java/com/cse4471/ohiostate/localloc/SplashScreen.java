598705b5088ccfe0567fbd316d69f73192100524
Elisabeth Holtman <holtman.9@buckeyemail.osu.edu>
2015-06-28 13:07:49 -0400

    Initial commit

---
+++ app/src/main/java/com/cse4471/ohiostate/localloc/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.cse4471.ohiostate.localloc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * Created by Liz on 6/27/2015.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent("com.cse4471.ohiostate.localloc.MAINACTIVITY");
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
