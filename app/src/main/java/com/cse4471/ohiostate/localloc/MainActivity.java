44e66166767c5c0d625f7e26a5c3efcfc1ffc010
Elisabeth Holtman <holtman.9@buckeyemail.osu.edu>
2015-07-13 23:09:22 -0400

    changes button coloring

--- app/src/main/java/com/cse4471/ohiostate/localloc/MainActivity.java
+++ app/src/main/java/com/cse4471/ohiostate/localloc/MainActivity.java
@@ -1,12 +1,17 @@
 package com.cse4471.ohiostate.localloc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.StateListDrawable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,26 +19,68 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+//Previously extended appCompatActivity
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
+//    private DrawerLayout drawerLayout;
+//    private ListView drawerList;
 
-    private DrawerLayout drawerLayout;
-    private ListView drawerList;
+    public void buttonOnClick(View v){
+        Intent nextActivity;
+        v.setBackground(getResources().getDrawable(R.drawable.buttonHover));
+        switch (v.getId()) {
+            case R.id.Button1:
+                nextActivity = new Intent(MainActivity.this, createSafeZone.class);
+                break;
+            case R.id.Button2:
+                nextActivity = new Intent(MainActivity.this, modifySafeZone.class);
+                break;
+            case R.id.Button3:
+                nextActivity = new Intent(MainActivity.this, deleteSafeZone.class);
+                break;
+            case R.id.Button4:
+                nextActivity = new Intent(MainActivity.this, Settings.class);
+                break;
+            default:
+                nextActivity = new Intent();
+        }
+        startActivity(nextActivity);
+    }
+
+    View.OnHoverListener buttonHovering = new View.OnHoverListener(){
+
+        @Override
+        public boolean onHover(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_HOVER_ENTER:
+                    v.setBackgroundResource(R.drawable.buttonHover);
+                    break;
+                case MotionEvent.ACTION_HOVER_MOVE:
+                 //   mMessageTextView.setText("Move: " + event.getX() + ":" + event.getY());
+                    break;
+                case MotionEvent.ACTION_HOVER_EXIT:
+                    v.setBackgroundDrawable(getResources().getDrawable(R.drawable.button));
+                    break;
+            }
+            return false;
+        }
+    };
 
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        drawerList = (ListView)findViewById(R.id.drawer_list);
+    }
+}
+/*  //      drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+  //    drawerList = (ListView) findViewById(R.id.drawer_list);
 
         drawerList.setAdapter(new ArrayAdapter<>(
                 this,
                 R.layout.drawer_list_item,
                 getResources().getStringArray(R.array.menu_items)));
 
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+//        drawerList.setOnItemClickListener(new DrawerItemClickListener());
 //        Button Button1 = (Button)findViewById(R.id.Button1);
 //        Button Button2 = (Button)findViewById(R.id.Button2);
 //        Button Button3 = (Button)findViewById(R.id.Button3);
@@ -87,3 +134,4 @@ public class MainActivity extends AppCompatActivity {
 //        return super.onOptionsItemSelected(item);
 //    }
 }
+*/
