a1f7eb799314e6e13f0c998d2605853c8c0afe4a
YalithKBK <yalithkbk@yahoo.com>
2015-07-25 19:15:20 -0400

    Started adding documentation to the data methods

--- app/src/main/java/com/cse4471/ohiostate/localloc/Data.java
+++ app/src/main/java/com/cse4471/ohiostate/localloc/Data.java
@@ -19,6 +19,16 @@ public final class Data {
 
     private Context context;
 
+    /**
+     *
+     * @param mac - Mac address of the newly connected Bluetooth device
+     * @param deviceID - Newly connected device ID name
+     * @return 1 if device was successfully added, 0 otherwise
+     * @requires the mac address is not already in the database
+     * @ensures the new BLuetooth device is in the database
+     *
+     * This method adds a new Bluetooth connection to the database.
+     */
     public void AddToBluetooth(String mac, String deviceID) {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
@@ -36,6 +46,15 @@ public final class Data {
         db.close();
     }
 
+    /**
+     *
+     * @param ssid - The ssid of the Wifi connection to be added
+     * @return 1 if device was successfully added, 0 otherwise
+     * @requires the ssid is not already in the database
+     * @ensures the new Wifi connection is in the database
+     *
+     * This method adds a new Bluetooth connection to the database.
+     */
     public void AddToWiFi (String ssid) {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
@@ -52,6 +71,15 @@ public final class Data {
         db.close();
     }
 
+    /**
+     *
+     * @param mac - Mac address of the Bluetooth commection to remove
+     * @return 1 if device was successfully revmoed, 0 otherwise
+     * @requires the mac address is in the database
+     * @ensures the BLuetooth device is removed from the database
+     *
+     * This method removes a Bluetooth connection from the database.
+     */
     public void RemoveFromBluetooth(String mac) {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
@@ -65,6 +93,15 @@ public final class Data {
         db.close();
     }
 
+    /**
+     *
+     * @param ssid - ssid of the Wifi commection to remove
+     * @return 1 if device was successfully revmoed, 0 otherwise
+     * @requires the ssid is in the database
+     * @ensures the Wifi device is removed from the database
+     *
+     * This method removes a Wifi connection from the database.
+     */
     public void RemoveFromWifi(String ssid) {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
@@ -78,15 +115,23 @@ public final class Data {
         db.close();
     }
 
-    public void UpdateBluetooth(String mac, String newDeviceID) {
+    /**
+     *
+     * @param mac - Mac address of the Bluetooth commection to update
+     * @param deviceID - The new name of the Bluetooth device
+     * @return 1 if device was successfully revmoed, 0 otherwise
+     * @requires the mac address is in the database
+     * @ensures the BLuetooth deviceID is updated in the database
+     *
+     * This method updates the ID of  a Bluetooth connection in the database.
+     */
+    public void UpdateBluetooth(String mac, String deviceID) {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
         SQLiteDatabase db = DBHelper.getWritableDatabase();
 
-        String[] selectionArgs = {mac};
-
         ContentValues values = new ContentValues();
-        values.put(DataContract.BluetoothTable.COLUMN_DEVICE_ID, newDeviceID);
+        values.put(DataContract.BluetoothTable.COLUMN_DEVICE_ID, deviceID);
 
         int count = db.update(
                 DataContract.BluetoothTable.TABLE_NAME,
@@ -97,6 +142,14 @@ public final class Data {
         db.close();
     }
 
+    /**
+     *
+     * @return a map containing mac address, device ID pairs of all
+     * Bluetooth connections in the database
+     *
+     * This method returns a map of <mac address, device ID> pairs of all
+     * the BLuetooth connections in the database.
+     */
     public Map ListBluetooth() {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
@@ -125,11 +178,19 @@ public final class Data {
         }
 
         c.close();
-
+        db.close();
         return bluetoothList;
 
     }
 
+    /**
+     *
+     * @return an arraylist containing the ssid of all wifi
+     * connections in the database
+     *
+     * This method returns an arraylist of all the wifi
+     * connections in the database.
+     */
     public ArrayList ListWifi() {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
@@ -156,10 +217,17 @@ public final class Data {
         }
 
         c.close();
-
+        db.close();
         return wifiList;
     }
 
+    /**
+     *
+     * @param mac - the mac address of the Bluetooth connection
+     * @return true if the mac address is in the database, false if not
+     *
+     *
+     */
     public boolean IsInBluetooth(String mac) {
         SafeZoneDBHelper DBHelper = new SafeZoneDBHelper(context);
 
@@ -183,6 +251,8 @@ public final class Data {
             isInDB = true;
         }
 
+        c.close();
+        db.close();
         return isInDB;
     }
 
@@ -208,6 +278,9 @@ public final class Data {
         if (c.getCount() > 0) {
             isInDB = true;
         }
+
+        c.close();
+        db.close();
         return isInDB;
     }
 }
