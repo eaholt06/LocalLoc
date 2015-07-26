499ec42a44b111de70f512f92d4576ab37596040
Andrew Miller <amiller956@gmail.com>
2015-07-26 12:24:12 -0400

    LockScreen update

--- app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenMainActivity.java
+++ app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenMainActivity.java
@@ -1,7 +1,38 @@
 package com.cse4471.ohiostate.localloc;
 
-/**
- * Created by AC on 7/23/2015.
- */
-public class LockScreenMainActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+
+
+public class LockScreenMainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        openLockScreen();
+        startService(new Intent(this, LockScreenService.class));
+
+        setContentView(R.layout.lockscreenactivity_main);
+
+    }
+
+    public void openLockScreen(){
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+        View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    @Override
+    public void onBackPressed(){
+
+    }
+
+    public void unlockScreen(View view){
+        android.os.Process.killProcess((android.os.Process.myPid()));
+    }
 }
--- app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenService.java
+++ app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenService.java
@@ -1,7 +1,44 @@
 package com.cse4471.ohiostate.localloc;
 
-/**
- * Created by AC on 7/22/2015.
- */
-public class LockScreenService {
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+public class LockScreenService extends Service {
+
+    private BroadcastReceiver lockReceiver;
+
+    @Override
+    public IBinder onBind(Intent intent){
+        return null;
+    }
+
+    @Override@SuppressWarnings("deprecation")
+    public void onCreate(){
+        KeyguardManager.KeyguardLock key;
+        KeyguardManager keym = (KeyguardManager)getSystemService(KEYGUARD_SERVICE);
+
+        key = keym.newKeyguardLock("IN");
+
+        key.disableKeyguard();
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_BOOT_COMPLETED);
+
+        lockReceiver = new LockScreenReceiver();
+        registerReceiver(lockReceiver, filter);
+
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy(){
+        unregisterReceiver(lockReceiver);
+        super.onDestroy();
+    }
+
 }
