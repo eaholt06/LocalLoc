5a5a268b7af14985c5aadbc328bc6d540cefb4bf
Andrew Miller <amiller956@gmail.com>
2015-07-28 09:43:09 -0400

    Lockscreen update

--- app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenMainActivity.java
+++ app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenMainActivity.java
@@ -19,104 +19,114 @@ public class LockScreenMainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
-
         boolean safe = ZoneChecker.updateZone(this);
+        if(safe){
+            Intent startMain = new Intent(Intent.ACTION_MAIN);
+            startMain.addCategory(Intent.CATEGORY_HOME);
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startMain);
+        }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mainText  = (TextView)findViewById(R.id.Pin);
-        openLockScreen();
-        startService(new Intent(this, LockScreenService.class));
-        final Button button1 = (Button)findViewById(R.id.button1);
-        final Button button2 = (Button)findViewById(R.id.button2);
-        final Button button3 = (Button)findViewById(R.id.button3);
-        final Button button4 = (Button)findViewById(R.id.button4);
-        final Button button5 = (Button)findViewById(R.id.button5);
-        final Button button6 = (Button)findViewById(R.id.button6);
-        final Button button7 = (Button)findViewById(R.id.button7);
-        final Button button8 = (Button)findViewById(R.id.button8);
-        final Button button9 = (Button)findViewById(R.id.button9);
-        final Button button0 = (Button)findViewById(R.id.button0);
-        final Button confirmButton = (Button)findViewById(R.id.confirm);
-        checkPin();
-        if(buttonPress.length() < pinLength){
-            button1.setOnClickListener(new View.OnClickListener() {
+        else{
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            mainText  = (TextView)findViewById(R.id.Pin);
+            openLockScreen();
+            startService(new Intent(this, LockScreenService.class));
+            final Button button1 = (Button)findViewById(R.id.button1);
+            final Button button2 = (Button)findViewById(R.id.button2);
+            final Button button3 = (Button)findViewById(R.id.button3);
+            final Button button4 = (Button)findViewById(R.id.button4);
+            final Button button5 = (Button)findViewById(R.id.button5);
+            final Button button6 = (Button)findViewById(R.id.button6);
+            final Button button7 = (Button)findViewById(R.id.button7);
+            final Button button8 = (Button)findViewById(R.id.button8);
+            final Button button9 = (Button)findViewById(R.id.button9);
+            final Button button0 = (Button)findViewById(R.id.button0);
+            final Button confirmButton = (Button)findViewById(R.id.confirm);
+            checkPin();
+            if(buttonPress.length() < pinLength){
+                button1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button1.getText().toString());
+                    }
+                });
+                button2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button2.getText().toString());
+                    }
+                });
+                button3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button3.getText().toString());
+                    }
+                });
+                button4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button4.getText().toString());
+                    }
+                });
+                button5.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button5.getText().toString());
+                    }
+                });
+                button6.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button6.getText().toString());
+                    }
+                });
+                button7.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button7.getText().toString());
+                    }
+                });
+                button8.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button8.getText().toString());
+                    }
+                });button9.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button9.getText().toString());
+                    }
+                });button0.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonPress = buttonPress.concat(button0.getText().toString());
+                    }
+                });
+            }
+
+            confirmButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button1.getText().toString());
-                }
-            });
-            button2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button2.getText().toString());
-                }
-            });
-            button3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button3.getText().toString());
-                }
-            });
-            button4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button4.getText().toString());
-                }
-            });
-            button5.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button5.getText().toString());
-                }
-            });
-            button6.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button6.getText().toString());
-                }
-            });
-            button7.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button7.getText().toString());
-                }
-            });
-            button8.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button8.getText().toString());
-                }
-            });button9.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button9.getText().toString());
-                }
-            });button0.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buttonPress = buttonPress.concat(button0.getText().toString());
+                    if(buttonPress.length() == pinLength && buttonPress.equals(masterP)){
+                        Intent startMain = new Intent(Intent.ACTION_MAIN);
+                        startMain.addCategory(Intent.CATEGORY_HOME);
+                        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(startMain);
+                        //this next line is meant to reset the lockscreen text
+                        mainText.setText("Enter PIN");
+                    }
+                    else {
+                        buttonPress = "";
+                        mainText.setText("Incorrect, Renter Pin");
+                    }
                 }
             });
         }
-
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(buttonPress.length() == pinLength && buttonPress.equals(masterP)){
-                    Intent startMain = new Intent(Intent.ACTION_MAIN);
-                    startMain.addCategory(Intent.CATEGORY_HOME);
-                    startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(startMain);
-                    mainText.setText("Enter PIN");
-                }
-                else {
-                    buttonPress = "";
-                    mainText.setText("Incorrect, Renter Pin");
-                }
-            }
-        });
     }
+
+
 
     public void openLockScreen() {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -144,6 +154,6 @@ public class LockScreenMainActivity extends AppCompatActivity{
 
     @Override
     public void onBackPressed() {
-
+        
     }
 }
--- app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenReceiver.java
+++ app/src/main/java/com/cse4471/ohiostate/localloc/LockScreenReceiver.java
@@ -11,10 +11,11 @@ public class LockScreenReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent){
         String action = intent.getAction();
 
-        if(action.equals(Intent.ACTION_SCREEN_OFF) || action.equals(Intent.ACTION_BOOT_COMPLETED)){
-            Intent i = new Intent(context, LockScreenMainActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(i);
+        if(action.equals(Intent.ACTION_SCREEN_OFF) ||
+                action.equals(Intent.ACTION_BOOT_COMPLETED)){
+            Intent nextActivity = new Intent(context, LockScreenMainActivity.class);
+            nextActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(nextActivity);
         }
     }
 }
